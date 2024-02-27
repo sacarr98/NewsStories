e46b8d0655143354843df987fcc32708c9c9b7e8
Sophie Carr <sacarr98@gmail.com>
2024-02-27 09:18:03 +0000

    create orfile model

--- yournews/admin.py
+++ yournews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+
+# unregister groups
+admin.site.unregister(Group)
+
+# extend user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Just display username fields on admin page
+    fields = ["username"]
+
+# unregister default user
+admin.site.unregister(User)
+# reregister User
+admin.site.register(User, UserAdmin)
