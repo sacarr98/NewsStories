2dd610083823fe57c109fed6ad1cad112687594b
Sophie Carr <sacarr98@gmail.com>
2024-02-27 10:52:56 +0000

    create function to automatically create a profile for new users that follows self

--- yournews/models.py
+++ yournews/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create User Profile Model
@@ -12,4 +13,15 @@ class Profile(models.Model):
         
     def __str__(self):
         return self.user.username
+
+# Create a profile for a new user
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # User follows themselves in order to view their own posts
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
 
