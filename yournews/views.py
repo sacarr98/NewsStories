29e66d8b8bf6b738263bbec1fcc3a81935649f74
Sophie Carr <sacarr98@gmail.com>
2024-02-27 11:05:52 +0000

    create page to display list of profiles

--- yournews/views.py
+++ yournews/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'home.html', {})
+
+
+def profile_list(request):
+    return render(request, 'profile_list.html')
