b2e9a4534da6dea320631765a966dc6ba7585de5
Sophie Carr <sacarr98@gmail.com>
2024-02-29 10:47:35 +0000

    create a follow and unfollow button on user profiles and write logic to allow users to change followers

--- yournews/views.py
+++ yournews/views.py
@@ -20,6 +20,21 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+
+        # form
+        if request.method == "POST":
+            # Get user
+            current_user_profile = request.user.profile
+            # Get form data
+            action = request.POST['follow']
+            # follow or unfollow a user
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+            # Save changes to profile
+            current_user_profile.save()
+            
     else:
         messages.success(request, ("Please log in to view this page"))
         return redirect('home')
