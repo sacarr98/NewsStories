0311c58036b30bed45319c4943de2981ae6c8450
Sophie Carr <sacarr98@gmail.com>
2024-02-29 11:40:13 +0000

    diplay users posts on users profiles

--- yournews/views.py
+++ yournews/views.py
@@ -23,6 +23,7 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        news = News.objects.filter(user_id=pk)
 
         # form
         if request.method == "POST":
@@ -42,6 +43,6 @@ def profile(request, pk):
         messages.success(request, ("Please log in to view this page"))
         return redirect('home')
 
-    return render(request, "profile.html", {"profile":profile})
+    return render(request, "profile.html", {"profile":profile, "news":news})
 
     
