31c884b3ab96dc1d3b62696f40a101687e35dcac
Sophie Carr <sacarr98@gmail.com>
2024-03-04 10:00:05 +0000

    create user login page and add login and logout links to navbar

--- yournews/views.py
+++ yournews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, News
 from .forms import NewsForm
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
@@ -58,3 +59,24 @@ def profile(request, pk):
     return render(request, "profile.html", {"profile":profile, "news":news})
 
     
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Error logging in, please try again"))
+            return redirect('login')
+    
+    else:
+        return render(request, "login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out successfully"))
+    return redirect('home')
