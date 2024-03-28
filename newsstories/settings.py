6f2cc8f4cdba8d3dc626dd888992c53eedc41a8c
Sophie Carr <sacarr98@gmail.com>
2024-03-28 10:51:35 +0000

    edit settings.py for automated testing

--- newsstories/settings.py
+++ newsstories/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -94,6 +95,9 @@ WSGI_APPLICATION = 'newsstories.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.gitpod.io",
